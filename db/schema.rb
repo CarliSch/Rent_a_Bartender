9f5ce45ac4069cf5a5c5ef731b12dd8bee9b7c90
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-15 17:30:35 +0100

    created bartender controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_141706) do
+ActiveRecord::Schema.define(version: 2021_11_15_153412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bartenders", force: :cascade do |t|
+    t.string "city"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bartenders_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "date"
+    t.bigint "user_id", null: false
+    t.bigint "bartender_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bartender_id"], name: "index_bookings_on_bartender_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +48,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_141706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bartenders", "users"
+  add_foreign_key "bookings", "bartenders"
+  add_foreign_key "bookings", "users"
 end
