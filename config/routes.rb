a96f18e6b5f57d524b7dd305ba5167931350c5e6
Ruben Asmus <ruben.berlin@icloud.com>
2021-11-18 14:22:24 +0100

    dashboard prototype

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :bartenders, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create]
 end
+resources :bookings, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
