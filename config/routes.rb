8ff3e1d2757573251c6f872664575d07de1c19f4
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-19 12:36:45 +0100

    reject and accept plus button in show and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :bartenders, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create]
-end
-resources :bookings, only: [:index]
+  end
+  resources :bookings, only: [:index]
+  patch "/accept", to: "bookings#accept", as: :accept
+  patch "/reject", to: "bookings#reject", as: :reject
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
